ec7ff968cee0a0f47537a136da1db86d24b07c9e
mo-xi-to <nastann76@gmail.com>
2025-12-11 14:55:11 +0700

    Add files via upload

---
+++ os_lab1/os_lab1/os_lab1.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+#include <cstdio>
+
+using namespace std;
+
+struct ComplexData {
+    int value;
+};
+
+mutex g_mutex;
+condition_variable g_cond;
+ComplexData* g_message = nullptr;
+bool g_ready = false;
+
+void run_provider() {
+    int id_counter = 0;
+
+    while (true) {
+        this_thread::sleep_for(chrono::seconds(1));
+
+        ComplexData* data = new ComplexData;
+        data->value = ++id_counter;
+
+        unique_lock<mutex> lock(g_mutex);
+
+        while (g_ready) {
+            g_cond.wait(lock);
+        }
+
+        g_message = data;
+        g_ready = true;
+
+        printf("Provider: sent event %d\n", data->value);
+
+        lock.unlock();
+        g_cond.notify_one();
+    }
+}
+
+void run_consumer() {
+    while (true) {
+        unique_lock<mutex> lock(g_mutex);
+
+        while (!g_ready) {
+            g_cond.wait(lock);
+        }
+
+        ComplexData* received = g_message;
+        g_message = nullptr;
+        g_ready = false;
+
+        printf("Consumer: processed event %d\n", received->value);
+
+        lock.unlock();
+        g_cond.notify_one();
+
+        delete received;
+    }
+}
+
+int main() {
+    thread t_prov(run_provider);
+    thread t_cons(run_consumer);
+
+    t_prov.join();
+    t_cons.join();
+
+    return 0;
+}
